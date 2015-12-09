87a2c6f1aeb23d599cb75ff3f760ec6c9887f2e4
JakobGrosen <sunfolk88@gmail.com>
2015-12-09 10:39:00 +0100

    Tilføjet howto.php

---
+++ pages/howto.php
@@ -0,0 +1,15 @@
+<?php
+    if(isset($_POST['url']) && $_POST['url'] == true) {
+        require "../include/ajax_includes.php";
+        require "../include/ajax/header_includes.php";
+    }
+ ?>
+
+<div style="width: 750px;margin: 0px auto 0px auto;">
+    
+    <h3><b>Oh, hi there!</b></h3> 
+    If you are here, you are either curious about the content of this page<br />
+    or you are uncertain of how <b>MIXR</b> works!<br />
+    If the latter is the case, you have come to the right place!<br />
+    On this page you will find all the relevant information to get you started with using <b>MIXR</b><br />
+</div>
